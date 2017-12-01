66889ec40c92e20df2aec1cc21c0dcb202f2235a
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2017-12-01 17:12:44 +0100

    Fixes to work with new code

--- test/kernels-tests.jl
+++ test/kernels-tests.jl
@@ -9,14 +9,15 @@ end
 include("../src/kernels.jl")
 
 function runtests()
-    for (name, ker) in (("Box", Kernels.box),
-                        ("Box (type)", Kernels.BoxKernel{Float32}),
-                        ("Catmull-Rom",  Kernels.catmull_rom),
-                        ("Mitchell-Netravali",  Kernels.mitchell_netravili),
+    for (name, ker) in (("RectangularSpline", Kernels.RectangularSpline()),
+                        ("RectangularSpline (type given)",
+                         Kernels.RectangularSpline(Float32)),
+                        #("Catmull-Rom",  Kernels.catmull_rom),
+                        #("Mitchell-Netravali",  Kernels.mitchell_netravili),
                         ("cardinal Mitchell-Netravali",
-                         Kernels.MitchellNetraviliKernel(Float16, 0, 1)),
+                         Kernels.MitchellNetraviliSpline(Float16, 0, 1)),
                         ("Duff's tensioned B-spline",
-                         Kernels.MitchellNetraviliKernel(Float32, 0.5, 0)))
+                         Kernels.MitchellNetraviliSpline(Float32, 0.5, 0)))
         println(name," kernel:")
         println(" - support: ", length(ker))
         println(" - normalized: ", Kernels.isnormalized(ker))
@@ -24,20 +25,27 @@ function runtests()
         println(" - ker(0): ", ker(0)) # test conversion
     end
 
+    box = Kernels.RectangularSpline()
+    triangle = Kernels.LinearSpline()
+    quadratic = Kernels.QuadraticSpline()
+    cubic = Kernels.CubicSpline()
+    catmull_rom = Kernels.CatmullRomSpline()
+    mitchell_netravili = Kernels.MitchellNetraviliSpline()
+
     plt.figure(1)
     plt.clf()
     x = linspace(-5,5,1000);
-    plt.plot(x, Kernels.box(x), color="darkgreen",
+    plt.plot(x, box(x), color="darkgreen",
              linewidth=2.0, linestyle="-");
-    plt.plot(x, Kernels.triangle(x), color="darkblue",
+    plt.plot(x, triangle(x), color="darkblue",
              linewidth=2.0, linestyle="-");
-    plt.plot(x, Kernels.quadratic(x), color="darkcyan",
+    plt.plot(x, quadratic(x), color="darkcyan",
              linewidth=2.0, linestyle="-");
-    plt.plot(x, Kernels.cubic(x), color="firebrick",
+    plt.plot(x, cubic(x), color="firebrick",
              linewidth=2.0, linestyle="-");
-    plt.plot(x, Kernels.catmull_rom(x), color="orange",
+    plt.plot(x, catmull_rom(x), color="orange",
              linewidth=2.0, linestyle="-");
-    plt.plot(x, Kernels.mitchell_netravili(x), color="violet",
+    plt.plot(x, mitchell_netravili(x), color="violet",
              linewidth=2.0, linestyle="-");
     plt.title("Some kernel functions");
 end
