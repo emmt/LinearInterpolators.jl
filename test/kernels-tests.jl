b073f2bd08a181551eeaceabc612423317640e6b
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2018-02-19 11:21:27 +0100

    More extensive tests

--- test/kernels-tests.jl
+++ test/kernels-tests.jl
@@ -15,14 +15,20 @@ function runtests()
                         #("Catmull-Rom",  Kernels.catmull_rom),
                         #("Mitchell-Netravali",  Kernels.mitchell_netravili),
                         ("cardinal Mitchell-Netravali",
-                         Kernels.MitchellNetraviliSpline(Float16, 0, 1)),
+                         Kernels.MitchellNetraviliSpline(Float32, 0, 1)),
                         ("Duff's tensioned B-spline",
-                         Kernels.MitchellNetraviliSpline(Float32, 0.5, 0)))
+                         Kernels.MitchellNetraviliSpline(Float32, 0.5, 0)),
+                        ("Lanczos 4 interpolation function",
+                         Kernels.LanczosKernel(4)))
+        ker16 = ker(Float16)
+        ker32 = ker(Float32)
+        ker64 = ker(Float64)
         println(name," kernel:")
         println(" - support: ", length(ker))
         println(" - normalized: ", Kernels.isnormalized(ker))
         println(" - cardinal: ", Kernels.iscardinal(ker))
         println(" - ker(0): ", ker(0)) # test conversion
+        println(" - ker32(0.1) ≈ ker64(0.1): ", ker32(0.1) ≈ ker64(0.1))
     end
 
     box = Kernels.RectangularSpline()
@@ -31,6 +37,10 @@ function runtests()
     cubic = Kernels.CubicSpline()
     catmull_rom = Kernels.CatmullRomSpline()
     mitchell_netravili = Kernels.MitchellNetraviliSpline()
+    keys = Kernels.KeysSpline(0.5)
+    lanczos2 = Kernels.LanczosKernel(2)
+    lanczos4 = Kernels.LanczosKernel(4)
+    lanczos6 = Kernels.LanczosKernel(6)
 
     plt.figure(1)
     plt.clf()
@@ -47,7 +57,35 @@ function runtests()
              linewidth=2.0, linestyle="-");
     plt.plot(x, mitchell_netravili(x), color="violet",
              linewidth=2.0, linestyle="-");
+    plt.plot(x, lanczos6(x), color="black",
+             linewidth=2.0, linestyle="-");
     plt.title("Some kernel functions");
+
+    for ker in (lanczos2,)
+        for t in (0.0, 0.1, 0.2, 0.2, 0.4)
+            println(typeof(ker), ": ",
+                    ker.(t .+ (0,-1)) .≈ Kernels.getweights(ker, t))
+        end
+    end
+    for ker in (quadratic,)
+        for t in (0.0, 0.1, 0.2, 0.2, 0.4)
+            println(typeof(ker), ": ",
+                    ker.(t .+ (1,0,-1)) .≈ Kernels.getweights(ker, t))
+        end
+    end
+
+    for ker in (cubic, catmull_rom, mitchell_netravili, keys, lanczos4)
+        for t in (1e-6, 0.1, 0.2, 0.2, 0.4)
+            println(typeof(ker), ": ",
+                    ker.(t .+ (1,0,-1,-2)) .≈ Kernels.getweights(ker, t))
+        end
+    end
+    for ker in (lanczos6,)
+        for t in (1e-6, 0.1, 0.2, 0.2, 0.4)
+            println(typeof(ker), ": ",
+                    ker.(t .+ (2,1,0,-1,-2,-3)) .≈ Kernels.getweights(ker, t))
+        end
+    end
 end
 
 runtests()
