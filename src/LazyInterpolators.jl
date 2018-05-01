97a5510e74b8c58abdfb1c4ca96e28a2e435a91d
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2018-05-01 20:41:18 +0200

    Include files

--- src/LazyInterpolators.jl
+++ src/LazyInterpolators.jl
@@ -1,5 +1,20 @@
+#
+# LazyInterpolators.jl -
+#
+# Implement various interpolation methods as linear mappings.
+#
+#------------------------------------------------------------------------------
+#
+# This file is part of the LazyInterpolators package licensed under the MIT
+# "Expat" License.
+#
+# Copyright (C) 2016-2018, Éric Thiébaut.
+#
+
 module LazyInterpolators
 
-# package code goes here
+include("AffineTransforms.jl")
+include("kernels.jl")
+include("interpolations.jl")
 
 end # module
