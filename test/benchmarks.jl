8addad7374fb4fe59c66ca8b16cb7df05ca160f6
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2021-07-29 08:22:10 +0200

    Make all benchmarks work again

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -8,6 +8,18 @@ using BenchmarkTools
 mimeshow(io::IO, x) = show(io, MIME"text/plain"(), x)
 mimeshow(x) = mimeshow(stdout, x)
 
+prt(op::AbstractString, t) = begin
+    @printf("%s:\n", op)
+    mimeshow(t)
+end
+
+prt(op::AbstractString, t, nops::Integer) = begin
+    # times in nanoseconds, speed in Gigaflops
+    @printf("%s [%.6f Gflops]:\n", op, nops/minimum(t.times))
+    mimeshow(t)
+    println()
+end
+
 n1, n2 = 300, 200
 m1, m2 =  43,  29
 y = randn(n1, n2)
@@ -16,29 +28,27 @@ A1 = SparseUnidimensionalInterpolator(ker, 1, 1:n1, range(1,n1,length=m1))
 A2 = SparseUnidimensionalInterpolator(ker, 2, 1:n2, range(1,n2,length=m2))
 H = A1*A2
 
-if false
-    x = conjgrad(H'*H, H'*y; verb=true, ftol=1e-8, maxiter=50)
-    z = vcopy(y)
-    println("\n\nA1*x:")
-    mimeshow(@benchmark(A1*x))
-    println("\n\nA2*x:")
-    mimeshow(@benchmark(A2*x))
-    println("\n\nA2*A1*x:")
-    mimeshow(@benchmark(A2*A1*x))
-    println("\n\nA1*A2*x:")
-    mimeshow(@benchmark(A1*A2*x))
-    println("\n\n\nA1'*y:")
-    mimeshow(@benchmark(A1'*y))
-    println("\n\nA2'*y:")
-    mimeshow(@benchmark(A2'*y))
-    println("\n\nA2'*A1'*y:")
-    mimeshow(@benchmark(A2'*A1'*y))
-    println("\n\nA1'*A2'*y:")
-    mimeshow(@benchmark(A1'*A2'*y))
-    println("")
+function LazyAlgebra.vcreate(::Type{LazyAlgebra.Direct},
+                             ::Gram,
+                             x::AbstractArray{T,N},
+                             scratch::Bool) where {T<:AbstractFloat,N}
+    return similar(x)
 end
 
-units = "Gflops" # times in nanoseconds, speed in Gigaflops
+r1 = 2*length(A1.C)
+r2 = 2*length(A2.C)
+x = conjgrad(H'*H, H'*y; verb=true, ftol=1e-8, maxiter=50)
+z = vcopy(y)
+prt("A1*x",      @benchmark($A1*$x),        r1*m2)
+prt("A2*x",      @benchmark($A2*$x),        r2*m1)
+prt("A2*A1*x",   @benchmark($(A2*A1)*$x),   r1*m2 + r2*n1)
+prt("A1*A2*x",   @benchmark($(A1*A2)*$x),   r2*m1 + r1*n2)
+prt("A1'*y",     @benchmark($(A1')*$y),     r1*m2)
+prt("A2'*y",     @benchmark($(A2')*$y),     r2*m1)
+prt("A2'*A1'*y", @benchmark($(A2'*A1')*$y), r1*m2 + r2*n1)
+prt("A1'*A2'*y", @benchmark($(A1'*A2')*$y), r2*m1 + r1*n2)
+println("")
+
 n = 100
 pos = [1.1*i + 0.2*j for i in 1:n1, j in 1:n2]
 pmin, pmax = extrema(pos)
@@ -47,14 +57,7 @@ x = randn(n)
 y = randn(n1,n2)
 nops = 2*length(A.C)
 
-t = @benchmark(A*x)
-@printf("\nA*x [%.6f %s]:\n", nops/median(t.times), units)
-mimeshow(t)
-println("")
-
-t = @benchmark(A'*y)
-@printf("\nA'*y [%.6f %s]:\n", nops/median(t.times), units)
-mimeshow(t)
-println("")
+prt("A*x",  @benchmark($A*$x), nops)
+prt("A'*x", @benchmark($(A')*$y), nops)
 
 end # module
