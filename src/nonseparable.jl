ef0a46644c6712b4c075b61a2e1b317994904cee
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2021-07-29 08:24:41 +0200

    Fix comment

--- src/nonseparable.jl
+++ src/nonseparable.jl
@@ -16,7 +16,7 @@
 """
     TwoDimensionalTransformInterpolator(rows, cols, ker1, ker2, R)
 
-yields a linear mapping which interpolate its input of size `cols` to produce
+yields a linear mapping which interpolates its input of size `cols` to produce
 an output of size `rows` by 2-dimensional interpolation with kernels `ker1` and
 `ker2` along each dimension and applying the affine coordinate transform
 specified by `R`.
